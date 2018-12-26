ff4af6ca7387070ff1f8a944e9d245c3c54bf9ec
Usmon <usmonzaripov@gmail.com>
2018-12-26 12:19:12 +0500

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -14,7 +14,7 @@ function getIp()
 {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         $ip = $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
     } else {
         $ip = $_SERVER['REMOTE_ADDR'];
